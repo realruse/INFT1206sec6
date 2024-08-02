01a5695362699f14fd13feff9e4b5b8f562ff7d5
realruse <daniel.bholla@dcmail.ca>
2024-08-02 14:44:24 -0400

    Update main.js

--- assignment 4/main.js
+++ assignment 4/main.js
@@ -14,3 +14,24 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
 const insertY = ["the soup kitchen", "Disneyland", "the White House"]
 
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+    let newStory = storyText
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
